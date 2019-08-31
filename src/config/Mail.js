2d51680cc2a1f9bb9451425d588cd907c4541eb7
MoisesMorandini <moises-morandini@hotmail.com>
2019-08-31 13:01:23 -0300

    add send email for cancellation of appointment

---
+++ src/config/Mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "816e28919b63a5",
+    pass: "4f885408fff370"
+  },
+  default: {
+    from : 'Equipe GoBarber <noreplay@gobarber.com>'
+  }
+
+}
